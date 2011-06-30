2974545ad453b11d59f7e08cf904715a398ac970
Hakan Tandogan <hakan@gurkensalat.com>
2011-06-30 22:13:07 +0200

    More startup work

---
+++ __init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fileencoding=UTF-8:ts=4:sw=4:sta:et:sts=4:ai
+from __future__ import (unicode_literals, division, absolute_import,
+                        print_function)
+
+__license__ = 'GPL v3'
+__copyright__ = '2011, Hakan Tandogan <hakan@gurkensalat.com>'
+__docformat__ = 'restructuredtext en'
+
+from calibre.ebooks.metadata.sources.base import Source
+
+class Perrypedia(Source):
+
+    name = 'Perrypedia'
+    description = _('Downloads Metadata and covers from Perrypedia')
+    supported_platforms = ['windows', 'osx', 'linux']
+    author = 'Hakan Tandogan'
+    version = (1, 0, 0)
+    minimum_calibre_version = (0, 8, 0)
